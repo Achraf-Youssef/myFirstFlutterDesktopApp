5e28406b254ee84495df0ad45a099aa98c20e19e
Achraf-Youssef <100535820+Achraf-Youssef@users.noreply.github.com>
2022-10-13 08:18:01 +0100

    removed program.dart and task.dart

--- flutter_application/lib/program.dart
+++
@@ -1,6 +0,0 @@
-class Program {
-  String name;
-  String path;
-
-  Program(this.name, this.path);
-}
--- flutter_application/lib/task.dart
+++
@@ -1,8 +0,0 @@
-class Task {
-  String title;
-  String subtitle;
-  String date;
-  List<Map> programList;
-
-  Task(this.title, this.subtitle, this.date, this.programList);
-}
